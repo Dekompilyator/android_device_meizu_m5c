d3e3bb804a96024571215ca06cead64d64242453
Dekompilyator <bzigd2023nohack@gmail.com>
2026-05-25 12:01:56 +0300

    Update configurations
    Add makefile for OMX
    
    Update sensors config with additional recommended params
    
    Add additional information in proprietary-files
    
    Remove jack server start in start-build.sh

--- start-build.sh
+++ start-build.sh
@@ -3,6 +3,5 @@ export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
 ./prebuilts/sdk/tools/jack-admin kill-server
-./prebuilts/sdk/tools/jack-admin start-server
 source build/envsetup.sh
 lunch lineage_m5c-userdebug && mka bacon
