a5f473bcbface7fc9eb55e670d36ae6653450997
Dekompilyator <bzigd2023nohack@gmail.com>
2026-06-24 14:58:21 +0300

    Fix patches path in "apply-patches.sh"

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 cd system/core
-git apply -v ../../device/meizu/m1710/patches/system_core.patch
+git apply -v ../../device/meizu/m5c/patches/system_core.patch
 cd ../..
 cd bionic
-git apply -v ../device/meizu/m1710/patches/bionic.patch
+git apply -v ../device/meizu/m5c/patches/bionic.patch
 cd ..
 cd system/sepolicy
-git apply -v ../../device/meizu/m1710/patches/system_sepolicy.patch
+git apply -v ../../device/meizu/m5c/patches/system_sepolicy.patch
 cd ../..
 cd frameworks/av
-git apply -v ../../device/meizu/m1710/patches/frameworks_av.patch
+git apply -v ../../device/meizu/m5c/patches/frameworks_av.patch
 cd ..
 cd native
-git apply -v ../../device/meizu/m1710/patches/frameworks_native.patch
+git apply -v ../../device/meizu/m5c/patches/frameworks_native.patch
 cd ..
 cd base
-git apply -v ../../device/meizu/m1710/patches/frameworks_base.patch
+git apply -v ../../device/meizu/m5c/patches/frameworks_base.patch
 cd ..
 cd opt/telephony
-git apply -v ../../../device/meizu/m1710/patches/frameworks_opt_telephony.patch
+git apply -v ../../../device/meizu/m5c/patches/frameworks_opt_telephony.patch
 cd ../../..
 cd packages/apps/Snap
-git apply -v ../../../device/meizu/m1710/patches/snap.patch
+git apply -v ../../../device/meizu/m5c/patches/snap.patch
 cd ..
 cd FMRadio
-git apply -v ../../../device/meizu/m1710/patches/fmradio.patch
+git apply -v ../../../device/meizu/m5c/patches/fmradio.patch
 cd ..
 cd Settings
-git apply -v ../../../device/meizu/m1710/patches/shivom.patch
+git apply -v ../../../device/meizu/m5c/patches/dekomp.patch
 cd ../../..
 cd system/netd
-git apply -v ../../device/meizu/m1710/patches/system_netd.patch
+git apply -v ../../device/meizu/m5c/patches/system_netd.patch
 cd ../..
 cd vendor/cmsdk
-git apply -v ../../device/meizu/m1710/patches/vendor_cmsdk.patch
+git apply -v ../../device/meizu/m5c/patches/vendor_cmsdk.patch
 cd ../..
 
